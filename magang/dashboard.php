0710038fb20c57ded8a9bf8e56a07d76d0593de5
Febri Ardana <73922828+FebriiArdn@users.noreply.github.com>
2022-09-08 16:03:20 +0700

    Update dashboard.php

--- magang/dashboard.php
+++ magang/dashboard.php
@@ -71,6 +71,46 @@ require 'cek.php';
                         <ol class="breadcrumb mb-4">
                             <li class="breadcrumb-item active">Dashboard</li>
                         </ol>
+                            <div class="card mb-4">
+                            <div class="card-header">
+                                <i class="fas fa-table mr-1"></i>
+                                Peta Lokasi Pos Hidrologi
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                        <thead>
+                                            <head>
+                                                <!-- Menyisipkan library Google Maps -->
+                                                <script src="http://maps.googleapis.com/maps/api/js"></script>
+                                            
+                                                <script>
+                                                    // fungsi initialize untuk mempersiapkan peta
+                                                    function initialize() {
+                                                    var propertiPeta = {
+                                                        center:new google.maps.LatLng(-7.120551031933386, 110.40485678699966),
+                                                        zoom:9,
+                                                        mapTypeId:google.maps.MapTypeId.ROADMAP
+                                                    };
+                                                    
+                                                    var peta = new google.maps.Map(document.getElementById("googleMap"), propertiPeta);
+                                                    }
+                                            
+                                                    // event jendela di-load  
+                                                    google.maps.event.addDomListener(window, 'load', initialize);
+                                                </script>
+                                              
+                                            </head>
+                                            <body>
+                                            
+                                                <!-- Elemen yang akan menjadi kontainer peta -->
+                                                <div id="googleMap" style="width:100%;height:380px;"></div>
+                                              
+                                            </body>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
                         <div class="row">
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-primary text-white mb-4">
@@ -109,131 +149,6 @@ require 'cek.php';
                                 </div>
                             </div>
                         </div>
-                        <div class="card mb-4">
-                            <div class="card-header">
-                                <!-- Button to Open the Modal -->
-                                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
-                                    Tambah Data
-                                </button>
-                            </div>
-                            <div class="card-body">
-                                <div class="table-responsive">
-                                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                                        <thead>
-                                            <tr>
-                                                <th>No.</th>
-                                                <th>Nama Pos</th>
-                                                <th>Jenis Pos</th>
-                                                <th>Lokasi Pos</th>
-                                                <th>Proggress</th>
-                                                <th>Change</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-
-                                            <?php
-                                            $ambilsemuadata = mysqli_query($conn,"select * from laporan");
-                                            $i = 1;
-                                            while($data=mysqli_fetch_array($ambilsemuadata)){
-                                                $nama_pos = $data['nama_pos'];
-                                                $jenis_pos = $data['jenis_pos'];
-                                                $lokasi_pos = $data['lokasi_pos'];
-                                                $proggres = $data['proggres'];
-                                                $id_pos = $data['id_pos'];
-                                            ?>
-
-                                            <tr>
-                                                <td><?=$i++;?></td>
-                                                <td><?=$nama_pos;?></td>
-                                                <td><?=$jenis_pos;?></td>
-                                                <td><?=$lokasi_pos;?></td>
-                                                <td><?=$proggres;?></td>
-                                                <td>
-                                                    <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#edit<?=$id_pos;?>">
-                                                        Edit
-                                                    </button>
-                                                    <input type="hidden" name="idposdihapus" value="<?=$id_pos;?>">
-                                                    <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#delete<?=$id_pos;?>">
-                                                        Delete
-                                                    </button>
-                                                </td>
-                                            </tr>
-
-                                            <!-- Edit Modal -->
-                                            <div class="modal fade" id="edit<?=$id_pos;?>">
-                                                <div class="modal-dialog">
-                                                <div class="modal-content">
-                                                
-                                                    <!-- Modal Header -->
-                                                    <div class="modal-header">
-                                                    <h4 class="modal-title">Edit data</h4>
-                                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                                    </div>
-                                                    
-                                                    <!-- Modal body -->
-                                                    <form method="post">
-                                                    <div class="modal-body">
-                                                    <input type="text" name="nama_pos" value="<?=$nama_pos;?>" class="form-control" required>
-                                                    <br>
-                                                    <input type="text" name="jenis_pos" value="<?=$jenis_pos;?>" class="form-control" required>
-                                                    <br>
-                                                    <input type="text" name="lokasi_pos" value="<?=$lokasi_pos;?>" class="form-control" required>
-                                                    <br>
-                                                    <input type="text" name="proggres" value="<?=$proggres;?>" class="form-control">
-                                                    <br>
-                                                    <button type="submit" class="btn btn-primary" name="update">Update</button> 
-                                                    </div>
-                                                    
-                                                </div>
-                                                </div>
-                                            </div>
-
-                                            <!-- Delete Modal -->
-                                            <div class="modal fade" id="delete<?=$id_pos;?>">
-                                                <div class="modal-dialog">
-                                                <div class="modal-content">
-                                                
-                                                    <!-- Modal Header -->
-                                                    <div class="modal-header">
-                                                    <h4 class="modal-title">Hapus data</h4>
-                                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                                    </div>
-                                                    
-                                                    <!-- Modal body -->
-                                                    <form method="post">
-                                                    <div class="modal-body">
-                                                    Apakah Anda yakin ingin menghapus data?
-                                                    <br>
-                                                    <br>
-                        
-                                                    <button type="submit" class="btn btn-danger" name="delete">Hapus</button> 
-                                                    </div>
-                                                    
-                                                </div>
-                                                </div>
-                                            </div>
-                                            <?php
-                                            };
-                                            ?>
-
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </main>
-                <footer class="py-4 bg-light mt-auto">
-                    <div class="container-fluid">
-                        <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Your Website 2020</div>
-                            <div>
-                                <a href="#">Privacy Policy</a>
-                                &middot;
-                                <a href="#">Terms &amp; Conditions</a>
-                            </div>
-                        </div>
-                    </div>
                 </footer>
             </div>
         </div>
@@ -247,41 +162,4 @@ require 'cek.php';
         <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
         <script src="assets/demo/datatables-demo.js"></script>
     </body>
-        <!-- The Modal -->
-    <div class="modal fade" id="myModal">
-        <div class="modal-dialog">
-        <div class="modal-content">
-        
-            <!-- Modal Header -->
-            <div class="modal-header">
-            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="32" fill="currentColor" class="bi bi-file-earmark-bar-graph" viewBox="0 0 16 16">
-                <path d="M10 13.5a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-6a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v6zm-2.5.5a.5.5 0 0 1-.5-.5v-4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5h-1zm-3 0a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5h-1z"></path>
-                <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z"></path>
-            </svg>
-            <h4 class="modal-title"> Tambah Data Pos</h4>
-            <button type="button" class="close" data-dismiss="modal">&times;</button>
-            </div>
-            
-            <!-- Modal body -->
-            <form method="post">
-            <div class="modal-body">
-            <input type='text' name='nama_pos' placeholder="Nama Pos" class="form-control" required>
-            <br>
-            <input type='text' name='jenis_pos' placeholder="Jenis Pos" class="form-control" required>
-            <br>
-            <input type='text' name='lokasi_pos' placeholder="Lokasi Pos" class="form-control" required>
-            <br>
-            <input type='text' name='proggres' placeholder="Proggres" class="form-control">
-            <br>
-            <button type="submit" class="btn btn-primary" name="tambahdata">Submit</button> 
-            </div>
-            
-            <!-- Modal footer -->
-            <div class="modal-footer">
-            <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-            </div>
-            
-        </div>
-        </div>
-    </div>
 </html>
