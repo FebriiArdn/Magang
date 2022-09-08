dff8f5199800ffbc375deaa9d8e5def7b0af7af6
Febri Ardana <73922828+FebriiArdn@users.noreply.github.com>
2022-09-08 16:07:49 +0700

    Update function.php

--- magang/function.php
+++ magang/function.php
@@ -4,7 +4,7 @@ session_start();
 //membuat koneksi ke database
 $conn = mysqli_connect("localhost", "root", "", "magang");
 
-//nambah barang
+//nambah data
 if(isset($_POST['tambahdata'])){
     $nama_pos = $_POST['nama_pos'];
     $jenis_pos = $_POST['jenis_pos'];
@@ -19,5 +19,23 @@ if(isset($_POST['tambahdata'])){
         header('location:index.php');
     }
 }
+
+//update data
+if(isset($_POST['updatedata'])){
+    $id_pos = $_POST['id_pos'];
+    $nama_pos = $_POST['nama_pos'];
+    $jenis_pos = $_POST['jenis_pos'];
+    $lokasi_pos = $_POST['lokasi_pos'];
+    $proggres = $_POST['proggres'];
+
+    $update = mysqli_query($conn,"update laporan set nama_pos='$nama_pos', jenis_pos='$jenis_pos', lokasi_pos='$lokasi_pos',proggres='$proggres' where id_pos = '$id_pos'");
+    if($update){
+        header('location:index.php');
+    } else {
+        echo 'Gagal';
+        header('location:index.php');
+    }
+}
+
 
 ?>
