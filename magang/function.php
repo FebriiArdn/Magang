6f649c520df15036c6ff0d1e58799d693c7b1405
imronassidiqi <73922408+imronassidiqi@users.noreply.github.com>
2022-09-26 09:49:23 +0700

    Update function.php

--- magang/function.php
+++ magang/function.php
@@ -10,15 +10,68 @@ if(isset($_POST['addnewlap'])){
     $jenis_pos = $_POST['jenis_pos'];
     $lokasi_pos = $_POST['lokasi_pos'];
     $proggres = $_POST['proggres'];
+    
+    //soal gambar
+    $allowed_extension = array('png', 'jpg');
+    $nama = $_FILES['file']['name']; //ngambil nama
+    $dot = explode('.', $nama);
+    $ekstensi = strtolower(end($dot)); //ngambil ekstensi
+    $ukuran = $_FILES['file']['size'];
+    $file_tmp = $_FILES['file']['tmp_name']; //ngambil lok
 
-    $adddata = mysqli_query($conn,"insert into laporan (nama_pos, jenis_pos, lokasi_pos, proggres) values('$nama_pos','$jenis_pos','$lokasi_pos','$proggres')");
-    if($adddata){
-        header('location:index.php');
-    } else {
-        echo 'Gagal';
-        header('location:index.php');
+    //penamaan file > enskripsi
+    $image = md5(uniqid($nama,true) . time()).'.'. $ekstensi; //menggabungkan nama file yang dienkripsi
+
+
+    //validasi
+    $cek = mysqli_query($conn, "select * from laporan where nama_pos='$nama_pos'");
+    $hitung = mysqli_num_rows($cek);
+
+    if($hitung<1){
+        //jika belum ada
+
+        //proses upload gambar
+        if(in_array($ekstensi, $allowed_extension) === true){
+            //validasi ukuran file
+            if($ukuran < 15000000){
+                move_uploaded_file($file_tmp, 'image/'. $image);
+
+                $adddata = mysqli_query($conn,"insert into laporan (nama_pos, jenis_pos, lokasi_pos, proggres, image) values('$nama_pos','$jenis_pos','$lokasi_pos','$proggres', '$image')");
+                if($adddata){
+                    header('location:index.php');
+                } else {
+                    echo 'Gagal';
+                    header('location:index.php');
+                }
+            } else {
+                //file terlalu besar
+                echo '
+                <script>
+                alert("ukuran terlalu besar");
+                window.location.href="index.php";
+                </script>
+                ';
+
+            }
+        } else {
+            echo '
+            <script>
+            alert("file harus png/jpg");
+            window.location.href="index.php";
+            </script>
+            ';
+        }
+    }else {
+        //jika sudah ada 
+        echo '
+        <script>
+        alert("nama barang sudah terdaftar");
+        window.location.href="index.php";
+        </script>
+        ';
     }
-}
+};
+
 
 //update data
 if(isset($_POST['updatedata'])){
@@ -39,7 +92,12 @@ if(isset($_POST['updatedata'])){
 
 //menghapus
 if(isset($_POST['hapusdata'])){
-    $id_pos = $_POST['id_pos'];
+    $id_pos = $_POST['id_pos']; 
+
+    $gambar = mysqli_query($conn, "select * from laporan where id_pos='$id_pos'");
+    $get = mysqli_fetch_array($gambar);
+    $img = 'image/' .$get['image'];
+    unlink($img);
 
     $hapus = mysqli_query($conn, "delete from laporan where id_pos = '$id_pos'");
     if($update){
