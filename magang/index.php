d28e313184da9e8285808b841ed34dca49ee0521
Febri Ardana <73922828+FebriiArdn@users.noreply.github.com>
2022-10-07 15:31:21 +0700

    Update index.php

--- magang/index.php
+++ magang/index.php
@@ -251,8 +251,7 @@ $count4 = mysqli_num_rows($get4); //menghitung seluruh kolom
                                                     <div class="modal-body">
                                                     Nama Pos <input type="text" name="nama_pos" value="<?=$nama_pos;?>" class="form-control" required>
                                                     <label for="jenis_pos">Jenis Pos</label>
-                                                                <select name="jenis_pos" class="form-control" required>
-                                                                    <option value="<?=$data['jenis_pos']?>"><?=$data['jenis_pos']?></option>
+                                                                <select name="jenis_pos" value="<?=$data['jenis_pos']?>"<?=$data['jenis_pos']?> class="form-control" required>
                                                                     <option value="Pos Duga Air">Pos Duga Air</option>
                                                                     <option value="Pos Curah Hujan">Pos Curah Hujan</option>
                                                                     <option value="Pos Klimatologi">Pos Klimatologi</option>
